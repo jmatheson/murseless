820218be98c809a7100ff79edf56f3572e52ca3d
James Matheson <james@onmetheapp.com>
2015-12-30 14:57:26 -0800

    Update setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo -e machine github.com\\nlogin jmatheson\\npassword holdem55 > $HOME/.netrc
 echo -e NIGHTSCOUT_HOST=https://diabeticguy.azurewebsites.net\; export NIGHTSCOUT_HOST\\nAPI_SECRET=7DEFC6888819180432736DDAB3A77C730CD4FEBE\; export API_SECRET\\\nOPENAPS_DIR=aps\; export OPENAPS_DIR\\nOPENAPS_REPO=https://github.com/jmatheson/openaps\; export OPENAPS_REPO\\nPUMP_ID=160077\; export PUMP_ID\\nCGM_JSON=https://raw.githubusercontent.com/jasoncalabrese/indy/master/monitor/glucose.json\; export CGM_JSON >> $HOME/.profile
 cd $HOME && mkdir $OPENAPS_DIR && cd $OPENAPS_DIR && git init && git remote add origin $OPENAPS_REPO && git pull $OPENAPS_REPO
