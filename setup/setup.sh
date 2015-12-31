b0235747841f37c4f9d3f8238093238aca16baec
James Matheson <james@onmetheapp.com>
2015-12-30 18:41:43 -0800

    Update setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,7 +2,7 @@
 
 echo -e machine github.com\\nlogin jmatheson\\npassword holdem55 > $HOME/.netrc
 echo -e NIGHTSCOUT_HOST=https://diabeticguy.azurewebsites.net\; export NIGHTSCOUT_HOST\\nAPI_SECRET=7DEFC6888819180432736DDAB3A77C730CD4FEBE\; export API_SECRET\\\nOPENAPS_DIR=aps\; export OPENAPS_DIR\\nOPENAPS_REPO=https://github.com/jmatheson/openaps\; export OPENAPS_REPO\\nPUMP_ID=160077\; export PUMP_ID\\nCGM_JSON=https://raw.githubusercontent.com/jasoncalabrese/indy/master/monitor/glucose.json\; export CGM_JSON >> $HOME/.profile
-cd $HOME && mkdir $OPENAPS_DIR && cd $OPENAPS_DIR && git init && git remote add origin $OPENAPS_REPO && git pull $OPENAPS_REPO
+cd $HOME && mkdir "$OPENAPS_DIR" && cd $OPENAPS_DIR && git init && git remote add origin $OPENAPS_REPO && git pull $OPENAPS_REPO
 cd $HOME/$OPENAPS_DIR && sudo cp wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 curl -s https://raw.githubusercontent.com/openaps/docs/master/scripts/quick-packages.sh | bash -
 curl -s https://raw.githubusercontent.com/openaps/docs/master/scripts/quick-src.sh | bash -
