8720c3d4ca99e2c64a185ce5f6cf5706c8a1eb59
Jim Matheson <james@onmetheapp.com>
2016-01-28 15:41:54 -0800

    openaps-report invoke monitor/glucose.json
          TODO: better change descriptions
          /usr/local/bin/openaps-report invoke monitor/glucose.json

--- setup/fakebg.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo -e \\n    {\\n\\t\"direction\": "Flat",\\n\\t\"noise\": 1,\\n\\t\"dateString\": 1,\\n\\t\"sgv\": 105,\\n\\t\"date\": 1452551161736,\\n\\t\"unfiltered\": 145000,\\n\\t\"rssi\": 100,\\n\\t\"device\": \"JCMTEST\",\\n\\t\"filtered\": 145000,\\n\\t\"_id\": 1,\\n\\t\"type\": \"sgv\"\\n    } > $HOME/$OPENAPS_DIR/g.json
